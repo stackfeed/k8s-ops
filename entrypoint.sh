62bb0ebc2182712fdb92e06db8fcf383e2798c91
Denis Baryshev <dennybaa@gmail.com>
2018-12-14 09:31:17 +0300

    zsh installation on build

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,18 +1,18 @@
 #!/bin/sh
 
-## copy skeleton into actual $HOME
-cwd=$(pwd) && cd /home/_home-skeleton_
-ls -a1 | sed '/^\(\.\|\.\.\)$/d' | xargs -I{} sh -c "test -e $HOME/{} || cp -ar {} ${HOME}"
-cd "${cwd}"
+if [ -d "$HOME" ]; then
+  ## copy skeleton into actual $HOME (one time operation)
+  cwd=$(pwd) && cd /home/_home-skeleton_
+  ls -a1 | sed '/^\(\.\|\.\.\)$/d' | xargs -I{} sh -c "test -e $HOME/{} || cp -ar {} ${HOME}"
+  cd "${cwd}"
 
-## install oh-my-zsh and enable given plugins and theme
-if [ ! -d "$HOME/.oh-my-zsh" ]; then
-  sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+  ## set up zsh
+  awk '/^plugins=\(/,/\)/ { if ( $0 ~ /^plugins=\(/ ) print "plugins=('"${ZSH_PLUGINS}"')"; next } 1' $HOME/.zshrc > /tmp/.zshrc
+  mv /tmp/.zshrc $HOME/.zshrc
+  sed -i 's/\(ZSH_THEME\)=".*"/\1="'${ZSH_THEME}'"/' $HOME/.zshrc
+  sed -i 's/^# *\(DISABLE_AUTO_UPDATE=\).*/\1true/' $HOME/.zshrc
+  sed -i "s#\(export *ZSH=\).*#\1${HOME}/.oh-my-zsh#" $HOME/.zshrc
 fi
 
-## set zsh plugins and theme
-awk '/^plugins=\(/,/\)/ { if ( $0 ~ /^plugins=\(/ ) print "plugins=('"${ZSH_PLUGINS}"')"; next } 1' $HOME/.zshrc > /tmp/.zshrc
-mv /tmp/.zshrc $HOME/.zshrc && sed -i 's/\(ZSH_THEME\)=".*"/\1="'${ZSH_THEME}'"/' $HOME/.zshrc
-
 # execute command or fallback into shell
-[ -n "$*" ] && exec $@ || exec /bin/zsh
+[ -n "$*" ] && exec "$@" || exec /bin/zsh --login
